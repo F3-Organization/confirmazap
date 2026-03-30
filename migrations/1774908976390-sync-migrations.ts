4914c7a8fd56077f98fed0c2bcda1a37996e4cc1
Felipe Oliveira <flipeaz342@gmail.com>
2026-03-30 18:21:52 -0400

    refactor: consolidate and clean up database migration files

---
+++ migrations/1774908976390-sync-migrations.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class SyncMigrations1774908976390 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
